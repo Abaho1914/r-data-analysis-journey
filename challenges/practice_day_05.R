31d3019fed575b0f2ff7986de66cc7485d6d7cbd
Abaho Abbott Strange <2020mbr001@std.must.ac.ug>
2025-06-04 18:48:34 +0300

    Create practice_day_05.R
    Practice for day 05

---
+++ challenges/practice_day_05.R
@@ -0,0 +1,25 @@
+# 1. Create your own data frame of 5 medical students:
+
+medical_students <- data.frame(
+  Name =c("Abbott","Lillian","Joan","Ishimwe","Joy"),
+  Year = c("Year 1","Year 5","Year 4","Year 2","Year 3"),
+  Age = c(23,23,35,45,32),
+  Willing_to_Donate = c("Yes","No","Yes","Yes","No"),
+  stringsAsFactors = FALSE
+)
+str(medical_students)
+summary(medical_students)
+
+medical_students$Knowledge_Score  =c(12,5,9,13,7)
+summary(medical_students)
+
+knowledgable <- medical_students[medical_students$Knowledge_Score >= 12,]
+knowledgable
+
+medical_students_tbl <- as_tibble(medical_students)
+
+medical_students_tbl%>%
+  select(Name,Willing_to_Donate)
+
+
+
